d349ac3ed280e71d8b6efd251d04c4aa92947521
Sayfutdinov98 <ek1224111@gmail.com>
2023-06-01 18:21:10 +0400

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Максим Сайфутдинов on 01.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
